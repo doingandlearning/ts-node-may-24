82d36289b959e7ffd0ad0ad479356582dcd8e1e6
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-05-20 12:35:37 +0100

    adds updates

--- 02-exploring-node/src/02-urls.ts
+++ 02-exploring-node/src/02-urls.ts
@@ -1,13 +1,26 @@
 import http, { IncomingMessage, ServerResponse } from "node:http";
+import url from "node:url";
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    res.statusCode = 200;
+    const path = url.parse(req.url as string, true).path;
+    const query = url.parse(req.url as string, true).query;
     res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Hello, Companies House!!</h1>");
+
+    if (path === "/") {
+      res.write("Home Page");
+    } else if (path === "/about") {
+      res.write("About Page");
+    } else {
+      res.write("Not found");
+    }
+    res.statusCode = 200;
+    console.log(query.name);
+    // res.end("<h1>Hello, " + query.name + "!!</h1>");
+    res.end(`<h1>Hello, ${query.name}!!</h1>`);
   }
 );
 
