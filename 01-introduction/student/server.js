ec66f74667480dd4eb383977d5d6023b02ed35d3
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-05-20 11:51:03 +0100

    adds template

--- 01-introduction/student/server.js
+++ 01-introduction/student/server.js
@@ -1,14 +1,13 @@
-const http = require("http");
-
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = require("http");
+var hostname = "127.0.0.1";
+var port = 3000;
+var server = http_1.default.createServer(function (req, res) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello, World!\n");
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, function () {
+    console.log("Server running at http://".concat(hostname, ":").concat(port, "/"));
 });
