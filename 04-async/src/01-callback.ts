3ff9e59936ae72bc93f5d6508472044d0a11fb8b
Kevin Cunningham <github@kevincunningham.co.uk>
2024-05-22 09:56:47 +0100

    adds callback

--- 04-async/src/01-callback.ts
+++ 04-async/src/01-callback.ts
@@ -1,5 +1,39 @@
 import fs from "node:fs";
 
-fs.readFile("directors.json", "utf-8", (error: Error | null, data: string) => {
-  console.log(data);
-});
+// Callback Hell!
+fs.readFile(
+  "directors.json",
+  "utf-8",
+  (error: Error | null, directorsData: string) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const directors = directorsData;
+    fs.readFile(
+      "companies.json",
+      "utf-8",
+      (error: Error | null, companiesData: string) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        const companies = companiesData;
+        fs.readFile(
+          "projects.json",
+          "utf-8",
+          (error: Error | null, projectsData: string) => {
+            if (error) {
+              console.log(error);
+              return;
+            }
+            const projects = projectsData;
+            console.log(directors, companies, projects);
+          }
+        );
+      }
+    );
+  }
+);
+
+// Down here!
