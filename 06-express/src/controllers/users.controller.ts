e6514cd9c1a605428e7552c93d248bb0d298a1c8
Kevin Cunningham <github@kevincunningham.co.uk>
2024-05-22 15:06:10 +0100

    adds controller

--- 06-express/src/controllers/users.controller.ts
+++ 06-express/src/controllers/users.controller.ts
@@ -1,7 +1,18 @@
 import { Request, Response, NextFunction } from "express";
+import { User } from "../models/users.model";
+
+const users: User[] = [];
 
 export function createUser(req: Request, res: Response, next: NextFunction) {
-  res.send("CreateUser not implemented yet");
+  const user: User = req.body;
+  if (!user.location || !user.name) {
+    return res
+      .status(400)
+      .json({ message: "You need to send the location and name" });
+  }
+  user.id = String(users.length + 1);
+  users.push(user);
+  res.json(user);
 }
 
 // export default { createUser };
