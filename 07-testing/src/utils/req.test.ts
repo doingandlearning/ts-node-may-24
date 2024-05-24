ae892f8d0958bf0f4b3b529018c01477193a1d06
Kevin Cunningham <github@kevincunningham.co.uk>
2024-05-24 10:38:22 +0100

    adds the test

--- 07-testing/src/utils/req.test.ts
+++ 07-testing/src/utils/req.test.ts
@@ -1,3 +1,11 @@
 import { fakeRequest } from "./req";
 
-it("get a valid response from a non error url", async () => {});
+jest.setTimeout(30000);
+jest.useFakeTimers();
+
+it("get a valid response from a non error url", async () => {
+  const responsePromise = fakeRequest("https://hmrc.gov.uk");
+  jest.advanceTimersByTime(5000); // setTimeout
+  const response = await responsePromise;
+  expect(response).toMatchObject({ message: "Valid url" }); // Partial
+});
--- 07-testing/src/utils/req.ts
+++ 07-testing/src/utils/req.ts
@@ -1,9 +1,11 @@
-import { setTimeout } from "timers/promises";
-
 export async function fakeRequest(url: string) {
-  await setTimeout(100);
+  // Your own timeout Promise => compatible with Jest Fake timers
+  await new Promise((resolve, reject) => {
+    setTimeout(() => resolve(""), 3000);
+  });
+
   if (url === "http://error.com") {
     throw new Error("network error");
   }
-  return { message: "Valid url" };
+  return { message: "Valid url", status: 200 };
 }
