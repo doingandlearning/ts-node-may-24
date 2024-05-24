dae89c09d4040d923f857fe1cfc0ee1f8116c826
Kevin Cunningham <github@kevincunningham.co.uk>
2024-05-24 10:43:57 +0100

    push

--- 07-testing/student/src/store-async.ts
+++ 07-testing/student/src/store-async.ts
@@ -1,10 +1,11 @@
-import { setTimeout } from "timers/promises";
-
 export default async function store(value: Buffer) {
   if (Buffer.isBuffer(value) === false) {
     throw Error("input must be a buffer");
   }
-  await setTimeout(300);
+  await new Promise((resolve, reject) => {
+    setTimeout(() => resolve(""), 3000);
+  });
+
   const id = Math.random().toString(36).split(".")[1].slice(0, 4);
   return { id };
 }
