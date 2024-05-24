102f48c552b866b71c72ad6d38d1315ee9a6d969
Kevin Cunningham <github@kevincunningham.co.uk>
2024-05-24 15:55:39 +0100

    updates the database

--- 09-mongo-db/src/index.ts
+++ 09-mongo-db/src/index.ts
@@ -1,7 +1,18 @@
 import app from "./server";
-
+import connectToDb from "./utils/db";
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port http://localhost:${PORT}`);
-});
+async function startup() {
+  try {
+    console.log("Connecting to database ...");
+    await connectToDb();
+    console.log("Connected to database");
+    app.listen(PORT, () => {
+      console.log(`Listening on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startup();
