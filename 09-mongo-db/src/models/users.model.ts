dd5a1447d2a4062c5900563ef49945c0a0570cf8
Kevin Cunningham <github@kevincunningham.co.uk>
2024-05-24 16:06:21 +0100

    adds the model

--- 09-mongo-db/src/models/users.model.ts
+++ 09-mongo-db/src/models/users.model.ts
@@ -1,6 +1,16 @@
-export interface User {
-  name: string;
-  location: string;
-  role?: string;
-  id: string;
-}
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { required: true, type: String },
+  location: { required: true, type: String },
+  role: String,
+});
+
+export const User = mongoose.model("User", userSchema);
+
+// export interface User {
+//   name: string;
+//   location: string;
+//   role?: string;
+//   id: string;
+// }
